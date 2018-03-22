8ffe6cb28bf7ffcc2109589064f3b92ce7f0aad3
rlourenco <rlourenco@tce.pb.gov.br>
2018-03-22 13:54:24 -0300

    test fix umlPath problem

--- index.js
+++ index.js
@@ -58,7 +58,9 @@ module.exports = {
 			if (config.language != undefined) {
 				options.language = config.languague;
 			}
+			console.log("options.umlPath ", options.umlPath);
 			var umlPath = output.resolve(options.umlPath);
+			console.log('umlPath after ', umlPath);
 			mkdir('-p', umlPath);
 		},
 		"page:before": function (page) {
